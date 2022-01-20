7e4f53ffa0b504774255cd8b77c2c3595170fba2
Can Göktas <can.goektas@gmail.com>
2022-01-20 15:02:43 +0100

    Trigger deployment

--- src/polyfill.ts
+++ src/polyfill.ts
@@ -1,7 +1,7 @@
 import "core-js/es/promise";
 import "core-js/es/symbol";
-import "core-js/stable/set";
 import "core-js/modules/es.object.values";
 import "core-js/modules/es.object.entries";
+import "core-js/stable/set";
 import "core-js/es/string/starts-with";
 import "core-js/es/array/find";
