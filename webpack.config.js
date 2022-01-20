0cc8e77bf3c49038d198eadf508db1b8cae75008
Can Göktas <can.goektas@gmail.com>
2022-01-20 10:54:13 +0100

    Move output files to root dir

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   devtool: false,
   output: {
     filename: "app.js",
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname),
   },
   module: {
     rules: [
